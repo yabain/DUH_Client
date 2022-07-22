07267befb3c43f9f0fca9945d68d787e37513fd0
Cedric-Yaba-In <c.nguendap@yabain.com>
2022-07-22 10:18:18 +0100

    update server api link

---
+++ build/precache-manifest.b5feaa2b924bff44ec8ccfc58d4ae94b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "6953379caae41d8c8bc0",
+    "url": "https://web.douhave.co/static/css/main.83981ef4.chunk.css"
+  },
+  {
+    "revision": "6953379caae41d8c8bc0",
+    "url": "https://web.douhave.co/static/js/main.6953379c.chunk.js"
+  },
+  {
+    "revision": "cd1c57208c4fbb1729bc",
+    "url": "https://web.douhave.co/static/css/1.c4f4b9fd.chunk.css"
+  },
+  {
+    "revision": "cd1c57208c4fbb1729bc",
+    "url": "https://web.douhave.co/static/js/1.cd1c5720.chunk.js"
+  },
+  {
+    "revision": "af33e080ea37bf370848",
+    "url": "https://web.douhave.co/static/js/runtime~main.af33e080.js"
+  },
+  {
+    "revision": "83e8d0bb2fcc9c3162e26f93d1c36199",
+    "url": "https://web.douhave.co/static/media/Button- Submit.83e8d0bb.png"
+  },
+  {
+    "revision": "d89e5759c382a5e0aae660cb3efcbd83",
+    "url": "https://web.douhave.co/static/media/user3.d89e5759.jpg"
+  },
+  {
+    "revision": "295e8f2e171be3b7f35bed39eeff194e",
+    "url": "https://web.douhave.co/static/media/Button- Post It.295e8f2e.png"
+  },
+  {
+    "revision": "d827b81fe5a85e9ddc5b728bf2a78827",
+    "url": "https://web.douhave.co/static/media/Vintage clothes and accessories.d827b81f.png"
+  },
+  {
+    "revision": "7110288040490f05886419eda5c7cba0",
+    "url": "https://web.douhave.co/static/media/Collectibles.71102880.png"
+  },
+  {
+    "revision": "d4934448b2aa0a4cb73683dac0da7ac5",
+    "url": "https://web.douhave.co/static/media/Contact-Buyer-Gold.d4934448.png"
+  },
+  {
+    "revision": "3641032d15e4c54c941b3ff11c3e8c9d",
+    "url": "https://web.douhave.co/static/media/Share-Gold.3641032d.png"
+  },
+  {
+    "revision": "30f7afd9d79b0dddb54af33f11593c9b",
+    "url": "https://web.douhave.co/static/media/image-logo.30f7afd9.png"
+  },
+  {
+    "revision": "35ae200bd0a6c41fd0499c3b063f8839",
+    "url": "https://web.douhave.co/static/media/Top Bar- Gold.35ae200b.png"
+  },
+  {
+    "revision": "7d596c1c63c5b2b6724c01d0a3b99ffb",
+    "url": "https://web.douhave.co/static/media/LogoWbg.7d596c1c.png"
+  },
+  {
+    "revision": "0857ab10b0cecb572f93a37737addab6",
+    "url": "https://web.douhave.co/static/media/line-menu.0857ab10.svg"
+  },
+  {
+    "revision": "7884d30a21c9c101925158e8b1f7b0a7",
+    "url": "https://web.douhave.co/static/media/Sidebar- Black.7884d30a.png"
+  },
+  {
+    "revision": "7ee80fd10a088396aa130187648b847a",
+    "url": "https://web.douhave.co/static/media/Matches-Gold.7ee80fd1.png"
+  },
+  {
+    "revision": "be3a17e03ff5a5a62296bf3c33760ddd",
+    "url": "https://web.douhave.co/static/media/Post-Another-Gold.be3a17e0.png"
+  },
+  {
+    "revision": "718fab04dbc1a46294c7a2fc82a4786b",
+    "url": "https://web.douhave.co/static/media/Primary Logo- Pure Black.718fab04.png"
+  },
+  {
+    "revision": "a89f53e866ba44f054eca2d63ef24b30",
+    "url": "https://web.douhave.co/static/media/RIVERHACK_CYR.a89f53e8.otf"
+  },
+  {
+    "revision": "b1aed51f7a6c63efb554902440a47575",
+    "url": "https://web.douhave.co/static/media/RiverhackSwashes.b1aed51f.otf"
+  },
+  {
+    "revision": "e1996192b98a516646ff9a8c0c0ca90c",
+    "url": "https://web.douhave.co/static/media/Oswald-Regular.e1996192.ttf"
+  },
+  {
+    "revision": "58252c8c295695145b458c070ba6ecdb",
+    "url": "https://web.douhave.co/static/media/Oswald-Light.58252c8c.ttf"
+  },
+  {
+    "revision": "7c99afed2f8a0ac6129ae8c2d0308252",
+    "url": "https://web.douhave.co/index.html"
+  }
+];
